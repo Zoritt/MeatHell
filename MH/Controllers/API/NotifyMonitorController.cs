157a1dacb3bd689e15f52afc2ea9bf9c9ac4b56e
Paweł Życiński <zoritt@gmail.com>
2019-10-25 22:12:21 +0200

    REST Notify

---
+++ MH/Controllers/API/NotifyMonitorController.cs
@@ -0,0 +1,30 @@
+﻿using System.Linq;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using MH.Models;
+using MH.Data;
+
+namespace MH.Controllers.API
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NotifyMonitorController : ControllerBase
+    {
+        ApplicationDbContext _repo;
+
+        public NotifyMonitorController(ApplicationDbContext applicationDbContext)
+        {
+            _repo = applicationDbContext;
+        }
+
+        public IEnumerable<Notify> GetNotifys()
+        {
+            return _repo.Notifys.ToList();
+        }
+
+        ~NotifyMonitorController()
+        {
+            _repo.Dispose();
+        }
+    }
+}
