494ce7a72cefe26befacac6c99c209f2fa79aba5
Paweł Życiński <zoritt@gmail.com>
2019-10-25 22:46:26 +0200

    Data to Notifies

---
+++ MH/Data/Migrations/20191025201946_Notifys-Seed.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MH.Data.Migrations
+{
+    public partial class NotifysSeed : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("insert into Notifys values('Komunikat 1', GETDATE()), ('Komunikat 2', GETDATE())");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
