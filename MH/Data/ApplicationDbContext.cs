5a5c6612b4b2010e24714342766ea56dcf15df64
Paweł Życiński <zoritt@gmail.com>
2019-10-25 17:57:20 +0200

    initDB

--- MH/Data/ApplicationDbContext.cs
+++ MH/Data/ApplicationDbContext.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using MH.Models;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -8,6 +9,8 @@ namespace MH.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
+
+        public DbSet<Meal> Meals { get; set; }
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
