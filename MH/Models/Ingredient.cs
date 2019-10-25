283824a4bf4137ad0b83bb39772876317885e577
jnowaczyk <jacek.nowaczyk@gmail.com>
2019-10-25 18:11:52 +0200

    Ingredient

---
+++ MH/Models/Ingredient.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MH.Models
+{
+    public class Ingredient
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [Column(TypeName ="varchar(200)")]
+        public string Name { get; set; }
+    }
+}
