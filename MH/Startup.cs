6f783c23865031b10e90580b9e766431b775656e
Paweł Życiński <zoritt@gmail.com>
2019-10-25 19:48:02 +0200

    Core 2.2

--- MH/Startup.cs
+++ MH/Startup.cs
@@ -41,6 +41,7 @@ namespace MH
                 .AddEntityFrameworkStores<ApplicationDbContext>();
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+          
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
