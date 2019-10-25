4809c85b55b32fa1717d2649cb68fd9cf1a9df0b
jnowaczyk <jacek.nowaczyk@gmail.com>
2019-10-25 18:29:44 +0200

    Seed IngredientType_Seed

---
+++ MH/Data/Migrations/20191025162434_IngredientType_Seed.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MH.Data.Migrations
+{
+    public partial class IngredientType_Seed : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("insert IngredientType (Name) values('Wegańskie')");
+            migrationBuilder.Sql("insert IngredientType (Name) values('Wegetariańskie')");
+            migrationBuilder.Sql("insert IngredientType (Name) values('Klasyczne')");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
