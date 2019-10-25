97d491f40b3a8cfedf40256ffa617829f6511de9
Paweł Życiński <zoritt@gmail.com>
2019-10-25 17:35:28 +0200

    Meal class

---
+++ MH/Models/Meal.cs
@@ -0,0 +1,12 @@
+﻿
+namespace MH.Models
+{
+    public class Meal : AuditEntity
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string Description { get; set; }
+
+    }
+}
