f07fe215e90fb837dd8f044eb65926f61ba6f90e
jnowaczyk <jacek.nowaczyk@gmail.com>
2019-10-26 09:33:13 +0200

    Dodany kontrolek/widok pod testy front-a

---
+++ MH/Controllers/FrontEndController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MH.Controllers
+{
+    public class FrontEndController : Controller
+    {
+        public IActionResult FrontEndTesting()
+        {
+            return View("FrontEndTest");
+        }
+    }
+}
