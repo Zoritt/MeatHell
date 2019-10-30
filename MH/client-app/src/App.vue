f2ab0a7b9772e6cf2371c194d9be50278b9edbb6
Paweł Życiński <zoritt@gmail.com>
2019-10-30 18:18:39 +0100

    Pakiet vue

--- MH/client-app/src/App.vue
+++ MH/client-app/src/App.vue
@@ -1,76 +1,28 @@
 <template>
-    <div id="app">
-        <header>
-            <b-navbar toggleable="md" type="light" variant="light">
-                <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
-                <b-navbar-brand to="/">Food Tracker</b-navbar-brand>
-                <b-collapse is-nav id="nav-collapse">
-                    <b-navbar-nav>
-                        <b-nav-item href="#" @click.prevent="login" v-if="!user">Login</b-nav-item>
-                        <b-nav-item href="#" @click.prevent="logout" v-else>Logout</b-nav-item>
-                    </b-navbar-nav>
-                </b-collapse>
-            </b-navbar>
-        </header>
-        <main>
-            <router-view></router-view>
-        </main>
-    </div>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
 </template>
 
 <script>
+import HelloWorld from './components/HelloWorld.vue'
 
-    export default {
-        name: 'app',
-        data() {
-            return {
-                user: null
-            }
-        },
-        methods: {
-            login() {
-
-            },
-            async logout() {
-
-            }
-        }
-    }
+export default {
+  name: 'app',
+  components: {
+    HelloWorld
+  }
+}
 </script>
 
 <style>
-    body {
-        margin: 0;
-    }
-
-    #app {
-        font-family: 'Avenir', Helvetica, Arial, sans-serif;
-        -webkit-font-smoothing: antialiased;
-        -moz-osx-font-smoothing: grayscale;
-        color: #2c3e50;
-    }
-
-    main {
-        text-align: center;
-        margin-top: 40px;
-    }
-
-    header {
-        margin: 0;
-        height: 56px;
-        padding: 0 16px 0 24px;
-        background-color: #f8f9fa;
-        color: #ffffff;
-    }
-
-        header span {
-            display: block;
-            position: relative;
-            font-size: 20px;
-            line-height: 1;
-            letter-spacing: .02em;
-            font-weight: 400;
-            box-sizing: border-box;
-            padding-top: 16px;
-        }
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
 </style>
