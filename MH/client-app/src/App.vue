2aeeda9a8a06ef55a9d67d860ca5834399fa3161
Paweł Życiński <zoritt@gmail.com>
2019-10-31 19:41:56 +0100

    Filtrowanie i dodawanie tasków

--- MH/client-app/src/App.vue
+++ MH/client-app/src/App.vue
@@ -1,34 +1,65 @@
 <template>
   <div id="app">
-    <h4 class="bg-primary text-white text-center p-2">
-      Lista zadań użytkownika {{name}}
-    </h4>
+    <h4 class="bg-primary text-white text-center p-2">Lista zadań użytkownika {{name}}</h4>
     <div class="container-fluid p-4">
       <div class="row">
         <div class="col font-weight-bold">Zadanie</div>
         <div class="col-2 font-weight-bold">Zakończono?</div>
-    </div>
-    <div class="row" v-for="t in tasks" v-bind:key="t.action">
-      <div class="col">{{t.action}}</div>
-      <div class="col-2">
-        <input type="checkbox" v-model="t.done" class="form-check-input"/>
+      </div>
+      <div class="row" v-for="t in filteredTasks" v-bind:key="t.action">
+        <div class="col">{{t.action}}</div>
+        <div class="col-2 text-center">
+          <input type="checkbox" v-model="t.done" class="form-check-input" />
+          {{t.done}}
         </div>
+      </div>
+      <div class="row py-2">
+        <div class="col">
+          <input v-model="newItemText" class="form-control"/>
+        </div>
+        <div class="col-2">
+          <button class="btn btn-primary" v-on:click="addNewTodo">Dodaj</button>
+        </div>
+      </div>
+      <div class="row bg-secondary py-2 mt-2 text-white">
+        <div class="col text-center">
+          <input type="checkbox" v-model="hideCompleted" class="form-check-input" />
+          <label class="form-checl-label font-weight-body">Ukryj zakończone zadania</label>
+        </div>
+      </div>
     </div>
-  </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'app',
+  name: "app",
   data() {
     return {
-    name: "Adam",
-    tasks:[{action:"Kup kwiaty", done:false},
-    {action:"Znajdź buty", done:false},
-    {action:"Odbierz bilety", done:true},
-    {action:"Zadzwoń do Janks", done:false}]
-  }
-  }
-}
+      name: "Adam",
+      tasks: [
+        { action: "Kup kwiaty", done: false },
+        { action: "Znajdź buty", done: false },
+        { action: "Odbierz bilety", done: true },
+        { action: "Zadzwoń do Janka", done: false }
+      ],
+      hideCompleted: true,
+      newItemText:"",
+    };
+  },
+  
+  computed: {
+    filteredTasks() {
+      return this.hideCompleted ? this.tasks.filter(t => !t.done) : this.tasks;},      
+  },
+  methods: {
+    addNewTodo(){
+      this.tasks.push({
+        action:this.newItemText,
+        done:false
+      });
+      this.newItemText = "";
+    }
+  } 
+};
 </script>
