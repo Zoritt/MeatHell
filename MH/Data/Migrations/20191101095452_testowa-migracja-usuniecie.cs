9a6ca68ec23f59f1415645df70a1fd85d00472cd
Paweł Życiński <zoritt@gmail.com>
2019-11-01 11:52:08 +0100

    Nowy projekt Vue

---
+++ MH/Data/Migrations/20191101095452_testowa-migracja-usuniecie.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MH.Data.Migrations
+{
+    public partial class testowamigracjausuniecie : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Test",
+                table: "Notifys");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Test",
+                table: "Notifys",
+                nullable: true);
+        }
+    }
+}
---
+++ MH/htmlcssprimer/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h4 class="bg-primary text-white text-center p-2">
+      Lista zadań użytkownika Adam
+    </h4>
+    <div class="container-fluid p-4">
+      <div class="row">
+        <div class="col font-weight-bold">Zadanie</div>
+        <div class="col-2 font-weight-bold">Zakończono?</div>
+    </div>
+    <div class="row" v-for="t in tasks" v-bind:key="t.action">
+      <div class="col">{{t.action}}</div>
+      <div class="col-2">{{t.done}}</div>
+        </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data:function(){
+    return {
+      tasks:[{action:"Kup kwiaty", done:false},
+    {action:"Znajdź buty", done:false},
+    {action:"Odbierz bilety", done:true},
+    {action:"Zadzwoń do Janks", done:false}]
+    }
+  }
+}
+</script>
+
