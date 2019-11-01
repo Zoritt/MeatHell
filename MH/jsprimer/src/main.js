8d2eb2b26520b4ebd529565206eaaface4cadfd4
Paweł Życiński <zoritt@gmail.com>
2019-11-01 19:30:12 +0100

    Pętelki

--- MH/jsprimer/src/main.js
+++ MH/jsprimer/src/main.js
@@ -1,5 +1,7 @@
-function messageFunction(weather){        
-    let message = 'Dziś jest ${weather}.';
-    console.log(message);
-    }
-messageFunction('pochmurno');
+let myArray = [100, "Adam", true];
+for(let i=0; i < myArray.length; i++)
+{
+    console.log(myArray[i])
+}
+
+myArray.forEach((value, index) => console.log(`Indeks ${index}: ${value}`))
