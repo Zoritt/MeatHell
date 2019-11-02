d9ff48cd184886c8810676740115fd3f588af972
Paweł Życiński <zoritt@gmail.com>
2019-11-02 19:23:49 +0100

    Obsługa pliku index.js

--- MH/jsprimer/src/main.js
+++ MH/jsprimer/src/main.js
@@ -1,8 +1,8 @@
-import additionalFunction from "./maths/sum";
-//import {multiply, subtract as minus} from "./maths/operations"
-import * as ops from "./maths/operations"
+import * as math from "./maths"
 
 let values = [10,20,30,40,50];
-console.log(`Suma:${additionalFunction(values)}`);
-console.log(`Iloczyn:${ops.multiply(values)}`);
-console.log(`Różnica:${ops.subtract(1000,values)}`);
+
+console.log(`Suma:${math.addition(values)}`);
+console.log(`Iloczyn:${math.multiply(values)}`);
+console.log(`Różnica:${math.subtract(1000,values)}`);
+console.log(`Średnia:${math.mean(values)}`);
