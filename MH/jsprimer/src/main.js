e9548caa42d3171dfe6a9e3c43fce121046cc31e
Paweł Życiński <zoritt@gmail.com>
2019-11-02 16:57:22 +0100

    Sum -> reduce

--- MH/jsprimer/src/main.js
+++ MH/jsprimer/src/main.js
@@ -1,7 +1,9 @@
-let myArray = [100, "Adam", true];
-for(let i=0; i < myArray.length; i++)
-{
-    console.log(myArray[i])
-}
-
-myArray.forEach((value, index) => console.log(`Indeks ${index}: ${value}`))
+let produkts = [
+    {name:"Kapelusz", price: 24.5, stock: 10},
+    {name:"Kajak", price: 289.99, stock: 1},
+    {name:"Piłka", price: 10, stock: 0},
+    {name:"Buty do biegania", price: 116.50, stock: 20},
+];
+let totalValue = produkts.filter(item => item.stock>0)
+.reduce((prev, item) => prev + (item.price*item.stock),0);
+console.log(`Wartość łączna: ${totalValue.toFixed(2)} PLN`)
