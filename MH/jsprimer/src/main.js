850b35542ebacb233aa011ce77b96c3ecff3cab0
Paweł Życiński <zoritt@gmail.com>
2019-11-02 19:27:43 +0100

    Zmiana nazw funkcji z modułów

--- MH/jsprimer/src/main.js
+++ MH/jsprimer/src/main.js
@@ -1,8 +1,8 @@
-import * as math from "./maths"
+import {addition as add, multiply, subtract, mean as average} from "./maths"
 
 let values = [10,20,30,40,50];
 
-console.log(`Suma:${math.addition(values)}`);
-console.log(`Iloczyn:${math.multiply(values)}`);
-console.log(`Różnica:${math.subtract(1000,values)}`);
-console.log(`Średnia:${math.mean(values)}`);
+console.log(`Suma:${add(values)}`);
+console.log(`Iloczyn:${multiply(values)}`);
+console.log(`Różnica:${subtract(1000,values)}`);
+console.log(`Średnia:${average(values)}`);
