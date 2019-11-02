8f4777b020578f16e6bf402f2d421db1a7675448
Paweł Życiński <zoritt@gmail.com>
2019-11-02 20:01:22 +0100

    await i async

--- MH/jsprimer/src/main.js
+++ MH/jsprimer/src/main.js
@@ -2,4 +2,10 @@ import {asyncAdd} from "./maths"
 
 let values = [10,20,30,40,50];
 
-asyncAdd(values).then(total => console.log(`Suma łączna:${total}`));
+async function doTask(){
+    let total = await asyncAdd(values);
+    console.log(`Suma łączna:${total}`)
+}
+doTask();
+
+
