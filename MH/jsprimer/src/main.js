8c96577ac0924e8a442268007b05685c7661bd71
Paweł Życiński <zoritt@gmail.com>
2019-11-02 19:16:17 +0100

    Wiele modułów

--- MH/jsprimer/src/main.js
+++ MH/jsprimer/src/main.js
@@ -1,4 +1,8 @@
 import additionalFunction from "./maths/sum";
+//import {multiply, subtract as minus} from "./maths/operations"
+import * as ops from "./maths/operations"
+
 let values = [10,20,30,40,50];
-let total = additionalFunction(values);
-console.log(`Łącznie: ${total}`);
+console.log(`Suma:${additionalFunction(values)}`);
+console.log(`Iloczyn:${ops.multiply(values)}`);
+console.log(`Różnica:${ops.subtract(1000,values)}`);
