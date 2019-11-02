fdd6b1801752b36d11e62cfd2e9144da8b505df4
Paweł Życiński <zoritt@gmail.com>
2019-11-02 18:47:57 +0100

    Copy object

--- MH/jsprimer/src/main.js
+++ MH/jsprimer/src/main.js
@@ -1,9 +1,13 @@
-let produkts = [
-    {name:"Kapelusz", price: 24.5, stock: 10},
-    {name:"Kajak", price: 289.99, stock: 1},
-    {name:"Piłka", price: 10, stock: 0},
-    {name:"Buty do biegania", price: 116.50, stock: 20},
-];
-let totalValue = produkts.filter(item => item.stock>0)
-.reduce((prev, item) => prev + (item.price*item.stock),0);
-console.log(`Wartość łączna: ${totalValue.toFixed(2)} PLN`)
+let name = "Adam"
+let myData = {
+    name,
+    weather: "słonecznie",
+    printMessage() {
+        console.log(`Cześć, ${myData.name}!`);
+        console.log(`Dziś jest ${myData.weather}.`);
+    }
+};
+myData.printMessage();
+let secondObject = {};
+Object.assign(secondObject, myData);
+secondObject.printMessage();
