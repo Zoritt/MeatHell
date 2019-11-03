4ad99347f3108bd1bdaf7f241489d2c3cc131608
Paweł Życiński <zoritt@gmail.com>
2019-11-03 19:00:47 +0100

    Formatowanie -> currency

--- MH/sportsstore/src/components/ProductList.vue
+++ MH/sportsstore/src/components/ProductList.vue
@@ -3,8 +3,8 @@
       <div v-for="p in products" v-bind:key="p.id" class="card m-1 p-1 bg-light">
           <h4>
               {{p.name}}
-              <span class="badge badge-pill badge-promary float-right">
-                  {{p.price}}
+              <span class="badge badge-pill badge-primary float-right">
+                  {{p.price | currency}}
               </span>
           </h4>
           <div class="card-text bg-white p-1">{{p.description}}</div>
@@ -16,6 +16,11 @@ import {mapState} from "vuex";
 export default {
     computed:{
         ...mapState(["products"])
+    },
+    filters:{
+        currency(value){
+            return new Intl.NumberFormat("pl-PL", {style:"currency", currency: "PLN"}).format(value);
+        }
     }
 }
 </script>
